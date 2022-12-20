c6fc11563791c31e0b759b47129dd314430588d6
Moiwa <samuelmoiwa@gmail.com>
2022-12-20 01:37:48 -0800

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -6,3 +6,10 @@ name VARCHAR(256),
 date_of_birth DATE,
 primary key (id) );
 
+
+CREATE TABLE patients (
+id int GENERATED ALWAYS AS IDENTITY,
+type VARCHAR(256),name VARCHAR(256),
+primary key (id) );
+
+
