969701962ee65e879242b5a006fbeb1dd0712a99
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2022-12-20 15:36:02 +0530

    Update the file with other tables and join table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -7,9 +7,42 @@ date_of_birth DATE,
 primary key (id) );
 
 
-CREATE TABLE patients (
+CREATE TABLE treatments (
 id int GENERATED ALWAYS AS IDENTITY,
-type VARCHAR(256),name VARCHAR(256),
+type VARCHAR(256),
+name VARCHAR(256),
 primary key (id) );
 
 
+CREATE TABLE medical_histories (
+	id int GENERATED ALWAYS AS IDENTITY,
+	admitted_at TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
+	patient_id int,
+	status VARCHAR(256),
+    primary key (id),
+	foreign key (patient_id) REFERENCES patients (id)
+);
+
+CREATE TABLE medical_histories_treatments(
+	treatment_id int REFERENCES treatments (id),
+	medical_histories_id int REFERENCES medical_histories (id)	
+); 
+
+CREATE TABLE invoices (
+	id int GENERATED ALWAYS AS IDENTITY,
+	total_amount decimal,
+	generated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	payed_at TIMESTAMP,
+	medical_histories_id int UNIQUE REFERENCES medical_histories (id),
+	primary key (id)
+)
+
+CREATE TABLE invoice_items (
+	id int GENERATED ALWAYS AS IDENTITY,
+	unit_price decimal,
+	quantity int,
+	total_price decimal,
+	invoice_id int REFERENCES invoices (id),
+	treatment_id int REFERENCES treatments (id),
+	PRIMARY KEY (id)
+)
